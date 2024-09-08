6e6954862fd00c7a2f02ebb9780a6a311bab6bb2
songjimin <ssong503503@gmail.com>
2024-09-08 23:02:11 +0900

    main.jsx 수정

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import { createRoot } from 'react-dom/client'
+import { Routes, Route } from "react-router-dom"
+import Main from './pages/Main'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-    <App />
+    <Routes>
+      <Route path="/" element={<Main />} />
+    </Routes>
   </BrowserRouter>
 )
