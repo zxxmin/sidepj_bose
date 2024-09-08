ff3833a9cd572cfc1398cb27540c02bd64e70212
songjimin <ssong503503@gmail.com>
2024-09-08 23:10:50 +0900

    fix: 배포.... basename 설정

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,10 @@
 import { createRoot } from 'react-dom/client'
-import { Routes, Route } from "react-router-dom"
-import Main from './pages/Main'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Main />} />
-    </Routes>
+  <BrowserRouter basename="/sidepj_bose/">
+    <App />
   </BrowserRouter>
 )
