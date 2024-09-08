13e30fd1da9c2766b800949f56a195eafceb10ae
songjimin <ssong503503@gmail.com>
2024-09-08 22:53:38 +0900

    fix: vite.config.js 수정

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default {
+export default defineConfig({
   base: '/sidepj_bose/',
   build: {
     outDir: 'dist',
   },
   plugins: [react()],
-}
+});
