e4697d1a7711f0221ac1f2dc359bebf3f9427926
songjimin <ssong503503@gmail.com>
2024-09-10 08:32:29 +0900

    fix: 리다이랙션 주소 hostname에 따라 변경되게 설정

--- src/components/KakaoLogin.jsx
+++ src/components/KakaoLogin.jsx
@@ -2,9 +2,22 @@ import { useEffect } from "react"
 import { useNavigate } from "react-router-dom";
 import Button from "./Button";
 
+let REDIRECT_URI;
+let ServerName = window.location.hostname;
+console.log(ServerName)
+
+switch (ServerName) {
+    case 'localhost':
+        REDIRECT_URI = 'http://localhost:5173/sidepj_bose/successlogin';
+        break;
+    case 'zxxmin.github.io':
+        REDIRECT_URI = 'https://zxxmin.github.io/sidepj_bose/successlogin';
+        break;
+    default :
+        break;
+}
+
 const REST_API_KEY = '68bbec7aa1f63937ebb5311c52d8db22';
-// const REDIRECT_URI = 'http://localhost:5173/sidepj_bose/successlogin';
-const REDIRECT_URI = 'https://zxxmin.github.io/sidepj_bose/successlogin';
 const link = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
 
 const KakaoLogin = () => {
