90aefd10688589801f9cc8aa23a55293629cde16
songjimin <ssong503503@gmail.com>
2024-09-23 14:46:45 +0900

    오류 해결 복구

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import './index.css'
 import { BrowserRouter, HashRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter basename='/sidepj_bose/'>
+  <HashRouter>
     <App />
-  </BrowserRouter>
+  </HashRouter>
 )
