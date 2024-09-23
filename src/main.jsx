ebbc31157dad66a2b05459f1d71982275c0710a3
songjimin <ssong503503@gmail.com>
2024-09-23 13:33:21 +0900

    BrowswerRouter를 HashRouter로 변경

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, HashRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter basename="/sidepj_bose">
+  <HashRouter>
     <App />
-  </BrowserRouter>
+  </HashRouter>
 )
